4981e2156879c50fe4d278832a3d8914d879dee2
Mohsin Patwary <mohsin.eng@icloud.com>
2019-03-18 21:39:44 +0600

    project setup complete

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
-
-
-class WeatherViewController: UIViewController {
+//1 first step
+import CoreLocation
+// 2nd step to use cllocationmanagerdelegate
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "25ab4ff39d7e292ecd4f433a82aa3822"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
-    //TODO: Declare instance variables here
-    
+    //TODO: Declare instance variables here and innitialize without paramiter.
+    //3rd step to make object
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,7 +33,18 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         
         
-        //TODO:Set up the location manager here.
+        //TODO:Set up the location manager here. Cllocation manager delegate contain many method to collect the locaion information, we use delegate to reduce time using this method and functionality.
+           // 4th step delegate setup for find the location
+        
+            locationManager.delegate = self
+        
+            // Which location is the best to find the weather we have to find the area of a kilomiter of miter
+            // 5th step to find the circle of location
+        
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+            // 6step to find the user device authorization, when this apps use, this time it will run, this method will show in popup window.
+            locationManager.requestWhenInUseAuthorization()
     
         
         
